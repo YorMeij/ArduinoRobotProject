b83e7a61eac9fa06b6632c178b1eb354544c3182
Yoram Meijaard <y.j.meijaard@student.tue.nl>
2015-10-01 14:51:35 +0200

    New test file
    Tests wheater or not we can receive input from the control channel.

--- ControlTest.ino
+++ ControlTest.ino
@@ -1,11 +1,12 @@
 char incomingCharacter = 0; // Setting up a character variable
+int disp = 0;
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate
   pinMode(13, OUTPUT);
   digitalWrite(13,LOW);
 }
 void loop() {
-  delay(1000);
+
   // Check if something is being received
   if (Serial.available() > 0) {
     // read the incoming character:
@@ -13,12 +14,18 @@ void loop() {
     //Serial.println("OK!"); // Send OK back to laptop
     switch(incomingCharacter){
       case 'a': // Case block
-      digitalWrite(13,HIGH);
+		disp = 1;
       break;
       case 'b': // Case block
-      digitalWrite(13,LOW);
+		disp = 0;
       break;
     }
   } 
+  
+  if(disp > 0){
+		digitalWrite(13,HIGH);
+  } else {
+	  digitalWrite(13,LOW);
+  }  
 }
 
